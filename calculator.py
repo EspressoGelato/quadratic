db5453dadc074ffc44eb96d8424a8679bb337324
EspressoGelato <mayuchen0701@outlook.com>
2016-07-18 16:50:35 +0800

    commit

---
+++ calculator.py
@@ -0,0 +1,30 @@
+import math
+def judge:
+	a=input('a=')
+    b=input('b=')
+	c=input('c=')
+	parameter=[a,b,c]
+	for x in parameter:
+		if isinstance(x,float)=True:
+			class Digital(object):
+				def_init_(self,a,b,c,delta)：
+    			self.a = int(input('a='))
+   				self.b = int(input('b='))
+    			self.c = int(input('c='))
+    			delta = b*b-4*a*c
+    			return delta
+				def quadratic(delta,q,p,m,n):                                                                                              
+    			if delta<0:
+					print('no answer')
+    			elif delta=0
+					q=(-self.b)/2*self.a
+					print('the answer is = %s % (q))
+				else:
+        			p = math.sqrt(s)
+        			m = (-b+p)/2a
+        			n = (-b-p)/2a
+       				print('the answer is = %s and %s' % (m,n))
+      			return
+            else:
+				print('the parameter is not digital')
+    
